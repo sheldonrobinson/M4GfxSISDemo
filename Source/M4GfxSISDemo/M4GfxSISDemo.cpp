c1c99f564d3ec7f1479a3f30ab0d9ca390477750
Sheldon Robinson <sheldon.robinson@gmail.com>
2019-09-26 16:53:25 -0400

    Fixes linux build

--- Source/M4GfxSISDemo/M4GfxSISDemo.cpp
+++ Source/M4GfxSISDemo/M4GfxSISDemo.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2019 Sheldon Robinson. All Rights Reserved.
 
 #include "M4GfxSISDemo.h"
 #include "Modules/ModuleManager.h"
